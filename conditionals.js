4db06c4067a4a70b194ba1c2a1c22e288e50f76d
Ren Guidolin <8.renji@gmail.com>
2021-11-02 19:58:05 -0400

    more conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,24 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
 const temperature = 12
 if (temperature < 0){
   console.log ('scarf')
@@ -8,3 +29,20 @@ if (temperature < 0){
 }
 
 console.log('outside time')
+
+
+const isCitizen = true;
+const age = 26
+
+if (isCitizen && age > 18){
+  console.log('you cant vote');
+}
+
+if (temperature <-40 || temperature>40){
+  console.log('just stay inside man')
+}
+
+if (!raining){
+  console.log('no umbrella necessary')
+}
+
