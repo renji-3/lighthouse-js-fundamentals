694a4f9cffb04af6ceea326a7eec080373370947
Ren Guidolin <8.renji@gmail.com>
2021-11-02 19:51:30 -0400

    weather conditionals

---
+++ conditionals.js
@@ -0,0 +1,10 @@
+const temperature = 12
+if (temperature < 0){
+  console.log ('scarf')
+} else if (temperature < 15){
+  console.log('long sleeve')
+}else{
+  console.log('short sleeve')
+}
+
+console.log('outside time')
